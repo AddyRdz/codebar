1daf0a6fd01940614e8342648442b95fb1c7e851
Addy Rodriguez <rdz.33.a@gmail.com>
2022-02-18 16:29:54 -0600

    Part 1 finished

--- lib/app.py
+++ lib/app.py
@@ -1,1 +1,29 @@
 # Write your code here!
+
+class Member:
+    def __init__(self, full_name):
+        self.name = full_name
+     
+
+    def introduce(self):
+        print(f"Hi, my name is {self.name}")
+
+addy = Member ("Addy R.")
+print(addy.name)
+addy.introduce()
+
+class Student(Member):
+    def __init__(self, full_name, reason):
+        super().__init__(full_name)
+        self.reason = reason
+
+class Instructor(Member):
+    def __init__(self, full_name, bio, skills):
+        super().__init_(full_name)
+        self.bio = bio
+        self.skills = []
+    
+    def __init__(self, new_skill):
+        self.skills.append(new_skill)
+        
+    
