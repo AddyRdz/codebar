e4050e5ca11bf73f689dd72c61ddde8fb2d3770a
Addy Rodriguez <rdz.33.a@gmail.com>
2022-02-19 11:31:16 -0600

    adding workshop part

--- lib/app.py
+++ lib/app.py
@@ -12,18 +12,56 @@ addy = Member ("Addy R.")
 print(addy.name)
 addy.introduce()
 
+jane= Member("Jane Doe")
+print(jane.name) #, jane.reason#
+
+lena= Member("Lena Smith")
+print(lena.name)
+
 class Student(Member):
     def __init__(self, full_name, reason):
         super().__init__(full_name)
         self.reason = reason
 
+jane = Student("Jane Doe", "I am trying to learn programming and need some help")
+print(jane.reason)
+lena = Student("lena Smitch", "I am really excited about learning to program!")
+print(lena.reason)
+
 class Instructor(Member):
     def __init__(self, full_name, bio, skills):
         super().__init_(full_name)
         self.bio = bio
-        self.skills = []
+        self.skills = ["Python, Javascript, C++"]
     
-    def __init__(self, new_skill):
+# Vicky Ruby - HTML, JavaScript
+#    I want to help people learn coding.
+
+#  Nicole McMillan - Ruby
+#    I have been programming for 5 years in Ruby and want to spread the love
+
+    def add_skill(self, new_skill):
         self.skills.append(new_skill)
-        
+
+class Workshop:
+    def __init__ (self, date, subject):
+        self.date = date
+        self.subject = subject
+        self.instructors = []
+        self.students = []
+
+    def add_participant(self, participant):
+        if isinstance(participant, Student):
+            self.students.append(participant)
+
+        elif isinstance(participant, Instructor):
+            self.instructor.append(participant)
+
+        # def print_details
+        # .__class__
+        # .__dict__
+        # isinstance(...)
     
+workshop = Workshop("12/03/2014", "Shutl")
+workshop.add_participant(lena)
+print(workshop.students)
